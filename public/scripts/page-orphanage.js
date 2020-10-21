783e3bacdd4127fd05512dc8465dd465c50943b6
Vinicius <vinicius.nunes@grupo-tribuna.com>
2020-10-20 23:11:38 -0300

    aula 2 finalizada

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -22,3 +22,37 @@ const icon = L.icon({
 
 //Create and add marker
 L.marker([-23.9524935, -46.4029801], { icon }).addTo(map)
+
+
+//image galery
+
+function selectImage(event) {
+    const button = event.currentTarget;
+    console.log(button.children)
+
+    //Remover todas as classes ativas
+    const buttons = document.querySelectorAll(".images button")
+        //console.log(buttons)
+
+    buttons.forEach((button) => {
+        button.classList.remove("active");
+    })
+
+    // OU...
+
+    // buttons.forEach(removeActiveClass)
+
+    // function removeActiveClass(button){
+    //     button.classList.remove("active");
+    // }
+
+    //Selecionar a imagem clicada
+    const image = button.children[0];
+    const imageContainer = document.querySelector(".orphanage-details>img");
+
+    //Atualizar o container de imagem
+    imageContainer.src = image.src;
+
+    //Adicionar a classe .active para este botão clicado
+    button.classList.add("active")
+}
