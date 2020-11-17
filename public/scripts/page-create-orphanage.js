50fcbc06f7f7e955be3559e07644058d863a05cd
Vinicius <vinicius.nunes@grupo-tribuna.com>
2020-11-17 11:27:36 -0300

    aula 3 finalizada

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -1,64 +1,83 @@
 //create map
 
-const map = L.map('mapid').setView([-23.9524935, -46.4029801], 13);
+const map = L.map("mapid").setView([-23.9524935, -46.4029801], 13);
 //create and addd tileLayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // Create icon
 const icon = L.icon({
     iconUrl: "./public/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
+    popupAnchor: [170, 2],
+});
 
 let marker;
 
 //Create and add marker
-map.on('click', (event) => {
-
+map.on("click", (event) => {
     // console.log(event)
     const lat = event.latlng.lat;
     const lng = event.latlng.lng;
 
-    document.querySelector('[name = lat]').value = lat;
-    document.querySelector('[name = lng]').value = lng;
-
+    document.querySelector("[name = lat]").value = lat;
+    document.querySelector("[name = lng]").value = lng;
 
     //Remove icon
-    marker && map.removeLayer(marker)
+    marker && map.removeLayer(marker);
 
     //Add icon tileLayer
-    marker = L.marker([lat, lng], { icon })
-        .addTo(map)
-})
+    marker = L.marker([lat, lng], { icon }).addTo(map);
+});
 
 // photos uploader
 function addPhotoField() {
     //Pegar o container de fotos #images
-    const container = document.querySelector('#images');
+    const container = document.querySelector("#images");
     //Pegar o container para duplicar .new-image
-    const fieldsContainer = document.querySelectorAll('.new-upload');
+    const fieldsContainer = document.querySelectorAll(".new-upload");
     //Realizar o clone da última imagem adicionada.
-    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true);
+    const newFieldContainer = fieldsContainer[
+        fieldsContainer.length - 1
+    ].cloneNode(true);
     //Verificar se o campo está vazio, se sim, não adicionar ao container de imagens
     const input = newFieldContainer.children[0];
 
     if (input.value == "") {
-        return
+        return;
     }
     //Limpar o campo antes de adicionar ao container de imagens
     input.value = "";
     //Adicionar o clone ao container de #images
-    container.appendChild(newFieldContainer)
+    container.appendChild(newFieldContainer);
 }
 
 function deleteField(event) {
     //Verifica onde está sendo acionado o evento (SPAN)
-    const span = (event.currentTarget);
-    const fieldsContainer = document.querySelectorAll('.new-upload');
+    const span = event.currentTarget;
+    const fieldsContainer = document.querySelectorAll(".new-upload");
     if (fieldsContainer.length < 2) {
-        return
+        //Limpar o valor do campo
+        span.parentNode.children[0].value = "";
+        return;
     }
-    console.log('Cheguei aqui')
+    // Deletar o campo
+    span.parentNode.remove();
+}
+
+//Select yes or no
+function toggleSelect(event) {
+    //Retirar a class .active (dos botões)
+    document
+        .querySelectorAll(".button-select button")
+        .forEach((button) => button.classList.remove("active"));
+    //Colocar a class .active nesse botão clicado
+    const button = event.currentTarget;
+    button.classList.add('active');
+    //Pegar o botão clicado
+
+    //Atualizar o meu input hidden com o valor selecionado
+    const input = document.querySelector('[name="open_on_weekends"]')
+    input.value = button.dataset.value
+        //Retirar a class .active (dos botões)
 }
